506c47c1265544dc52ae3027f6f16102a6d4de77
Leandro-bep <jmartinezbarturen99@gmail.com>
2026-03-31 10:06:32 -0500

    dale

--- Java/areas/areas.java
+++ Java/areas/areas.java
@@ -1,19 +1,38 @@
 package areas;
 import java.util.Scanner;
+import java.util.ArrayList;
+
 
 public class areas {
     public static void main(String[]args){
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("ingrese un numero en formato texto;");
-        String numerotexto = sc.nextLine();
-
-        int numero = Integer.parseInt(numerotexto);
-
-        System.out.println("el numero ingresado en texto es " + numerotexto);
-        System.out.println("el numero ingresado en formato entero es " + numero);
         
-    }
+        Scanner sc = new Scanner(System.in);
+        
 
+        ArrayList<String> nombres = new ArrayList<>();
+        ArrayList<String> contraseñas = new ArrayList<>();
+        ArrayList<Integer> ids = new ArrayList<>();
+
+        System.out.println("ingrese su nombre :");
+        String nombre = sc.nextLine();
+
+        System.out.println("ingrese su contraseña: ");
+        String contraseña = sc.nextLine();
+
+        System.out.println("ingrese su id: ");
+        int id = sc.nextInt();
+
+        nombres.add(nombre);
+        contraseñas.add(contraseña);
+        ids.add(id);
+        
+
+        System.out.println("Nombre: " + nombres.get(0));
+        System.out.println("Contraseña: " + contraseñas.get(0));
+        System.out.println("ID: " + ids.get(0));
+
+        
+    }   
+    
 }
     
